505608fee2d143ff1e09db42a9b466c44315a7c8
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-28 21:33:11 +0545

    SMTP credential

--- traffic/create_pdf.php
+++ traffic/create_pdf.php
@@ -82,14 +82,14 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'np03cs4a210050@heraldcollege.edu.np';                 // SMTP username
-    $mail->Password   = '#L@zyBum655664';                   // SMTP password
+    $mail->Username   = '';                 // SMTP username
+    $mail->Password   = '';                   // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     //$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     // Set the email content
-    $mail->setFrom('np03cs4a210050@heraldcollege.edu.np', 'TMS');
+    $mail->setFrom('', 'TMS');
     $mail->addAddress($email);
     $mail->addStringAttachment($pdf_data, 'eChit.pdf');
     $mail->isHTML(true);
