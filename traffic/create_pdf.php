3bb744973cb5fea301b351061268f5679207bb26
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-28 18:51:57 +0545

    updated create_pdf

--- traffic/create_pdf.php
+++ traffic/create_pdf.php
@@ -1,142 +1,47 @@
 <?php
 
-// Include the necessary libraries
-require('fpdf/fpdf.php');
-
-// Get the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$date = $_POST['date'];
-$location = $_POST['location'];
-$vehicle_number = $_POST['vehicle_number'];
-$license_number = $_POST['license_number'];
-$category = $_POST['category'];
-$police_name = $_POST['police_name'];
-
-// Create a new PDF instance
-$pdf = new FPDF();
-
-// Add a page to the PDF
-$pdf->AddPage();
-
-// Set the font for the document
-$pdf->SetFont('Arial', 'B', 16);
-
-// Add the title to the document
-$pdf->Cell(0, 10, 'Vehicle Registration Form', 0, 1, 'C');
-
-// Set the font for the document
-$pdf->SetFont('Arial', '', 12);
-
-// Add the form data to the document
-$pdf->Cell(0, 10, 'Name: ' . $name, 0, 1);
-$pdf->Cell(0, 10, 'Email: ' . $email, 0, 1);
-$pdf->Cell(0, 10, 'Date: ' . $date, 0, 1);
-$pdf->Cell(0, 10, 'Location: ' . $location, 0, 1);
-$pdf->Cell(0, 10, 'Vehicle Number: ' . $vehicle_number, 0, 1);
-$pdf->Cell(0, 10, 'License Number: ' . $license_number, 0, 1);
-$pdf->Cell(0, 10, 'Category: ' . $category, 0, 1);
-$pdf->Cell(0, 10, 'Police Name: ' . $police_name, 0, 1);
-
-// Output the PDF
-$pdf->Output('D', 'vehicle-registration-form.pdf');
-
-?>
-
-
-----------------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx---------------------
-
-<?php
-// Include PHPMailer library
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
-// Database connection details
-$servername = "localhost:3307";
-$username = "root";
-$password = "";
-$dbname = "consumer";
-
-// Create a new database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check database connection
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-
-// Get email from HTML form
-$email = $_POST['email'];
-
-// Check if email exists in database table
-$sql = "SELECT * FROM users WHERE email='$email'";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // Email exists in database, generate 4-digit verification code
-    $verificationCode = rand(1000, 9999);
-
-    // Send verification code to email using PHPMailer
-    $mail = new PHPMailer(true);
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'np03cs4a210037@heraldcollege.edu.np';                     //SMTP username
-        $mail->Password   = 'october8563 herald';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to
-
-        //Recipients
-        $mail->setFrom('np03cs4a210037@heraldcollege.edu.np', 'Aqua Nepal');
-        $mail->addAddress($email);     //Add a recipient
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'Verification Code';
-        $mail->Body    = 'Your verification code is: ' . $verificationCode;
-
-        $mail->send();
-        echo 'Verification code sent to email: ' . $email;
-    } catch (Exception $e) {
-        echo "Verification code could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-} else {
-    // Email does not exist in database, show error message
-    echo 'Email does not exist in database.';
-}
-
-// Close database connection
-$conn->close();
-?>
-
-
-how to generate pdf of the user provided information in the form using php
-
-
-how to send the generated pdf to the email provided in the form
-
-
-
-<?php
 
 // Include the necessary libraries
+require('vendor/autoload.php');
 require('fpdf/fpdf.php');
-require('PHPMailer/PHPMailerAutoload.php');
 
 // Get the form data
 $name = $_POST['name'];
 $email = $_POST['email'];
 $date = $_POST['date'];
-$location = $_POST['location'];
 $vehicle_number = $_POST['vehicle_number'];
 $license_number = $_POST['license_number'];
-$category = $_POST['category'];
+$fine_category = $_POST['fine_category'];
+$fine_box = $_POST['fine_box'];
+$location = $_POST['location'];
+$traffic_station = $_POST['traffic_station'];
 $police_name = $_POST['police_name'];
+
+// Database connection
+$conn = new mysqli('localhost','root','','tms');
+if($conn->connect_error){
+    echo "$conn->connect_error";
+    die("Connection Failed : ". $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("insert into echit(name, email, date, vehicle_number, license_number, fine_category, fine_box, location, traffic_station, police_name) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssiisisss", $name, $email, $date, $vehicle_number, $license_number, $fine_category, $fine_box, $location, $traffic_station, $police_name);
+    $execval = $stmt->execute();
+    $stmt->close();
+    $conn->close();
+
+    if($execval) {
+        // show pop-up message
+        echo '<script>alert("data saved successfully!");</script>';
+        // redirect to login page
+        //echo '<script>window.location.href = "login.html";</script>';
+    } else {
+        echo '<script>alert("data saving failed!");</script>';
+        //echo "<script>alert('Error in registration'); window.location.href='signup.html';</script>";
+        //echo "Error in registration";
+    }
+}
 
 // Create a new PDF instance
 $pdf = new FPDF();
@@ -148,7 +53,7 @@ $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
 
 // Add the title to the document
-$pdf->Cell(0, 10, 'Vehicle Registration Form', 0, 1, 'C');
+$pdf->Cell(0, 10, 'eChit', 0, 1, 'C');
 
 // Set the font for the document
 $pdf->SetFont('Arial', '', 12);
@@ -157,33 +62,52 @@ $pdf->SetFont('Arial', '', 12);
 $pdf->Cell(0, 10, 'Name: ' . $name, 0, 1);
 $pdf->Cell(0, 10, 'Email: ' . $email, 0, 1);
 $pdf->Cell(0, 10, 'Date: ' . $date, 0, 1);
-$pdf->Cell(0, 10, 'Location: ' . $location, 0, 1);
 $pdf->Cell(0, 10, 'Vehicle Number: ' . $vehicle_number, 0, 1);
 $pdf->Cell(0, 10, 'License Number: ' . $license_number, 0, 1);
-$pdf->Cell(0, 10, 'Category: ' . $category, 0, 1);
+$pdf->Cell(0, 10, 'Fine Category: ' . $fine_category, 0, 1);
+$pdf->Cell(0, 10, 'Fine Box: ' . $fine_box, 0, 1);
+$pdf->Cell(0, 10, 'Location: ' . $location, 0, 1);
+$pdf->Cell(0, 10, 'Traffic Station: ' . $traffic_station, 0, 1);
 $pdf->Cell(0, 10, 'Police Name: ' . $police_name, 0, 1);
 
 // Output the PDF
 $pdf_data = $pdf->Output('S');
 
 // Create a new PHPMailer instance
-$mail = new PHPMailer;
+$mail = new PHPMailer(true);
 
-// Set the email parameters
-$mail->setFrom('you@example.com', 'Your Name');
-$mail->addAddress($email);
-$mail->addAttachment($pdf_data, 'vehicle-registration-form.pdf');
+try {
+    // Set the email parameters
+    $mail->SMTPDebug = 0;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'np03cs4a210050@heraldcollege.edu.np';                 // SMTP username
+    $mail->Password   = '#L@zyBum655664';                   // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    //$mail->SMTPSecure = 'tls';                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-// Set the email content
-$mail->Subject = 'Vehicle Registration Form';
-$mail->Body = 'Please find attached the vehicle registration form submitted by ' . $name . '.';
-$mail->AltBody = 'Please find attached the vehicle registration form submitted by ' . $name . '.';
+    // Set the email content
+    $mail->setFrom('np03cs4a210050@heraldcollege.edu.np', 'TMS');
+    $mail->addAddress($email);
+    $mail->addStringAttachment($pdf_data, 'eChit.pdf');
+    $mail->isHTML(true);
+    $mail->Subject = 'eChit details';
+    $mail->Body    = 'Please find attached eChit submitted by Traffic Management System';
+    $mail->AltBody = 'Please find attached eChit submitted by Traffic Management System';
 
-// Send the email
-if (!$mail->send()) {
-    echo 'Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Email sent!';
+    // Send the email
+    $mail->send();
+
+    // show pop-up message
+    echo '<script>alert("Message has been sent!");</script>';
+    // redirect to login page
+    echo '<script>window.location.href = "trafficdash.html";</script>';
+
+    //echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
 ?>
