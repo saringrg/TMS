e6b59b66a1a25ab4767e101d1efed7bda9f87654
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-29 23:34:38 +0545

    updated database conn

--- traffic/create_pdf.php
+++ traffic/create_pdf.php
@@ -19,3 +19,27 @@ $location = $_POST['location'];
 $traffic_station = $_POST['traffic_station'];
 $police_name = $_POST['police_name'];
 
+// Database connection
+$conn = new mysqli('localhost','root','','tms');
+if($conn->connect_error){
+    echo "$conn->connect_error";
+    die("Connection Failed : ". $conn->connect_error);
+} else {
+    $stmt = $conn->prepare("insert into echit(name, email, date, vehicle_number, license_number, fine_category, fine_box, location, traffic_station, police_name) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssiisisss", $name, $email, $date, $vehicle_number, $license_number, $fine_category, $fine_box, $location, $traffic_station, $police_name);
+    $execval = $stmt->execute();
+    $stmt->close();
+    $conn->close();
+
+    if($execval) {
+        // show pop-up message
+        echo '<script>alert("data saved successfully!");</script>';
+        // redirect to login page
+        //echo '<script>window.location.href = "login.html";</script>';
+    } else {
+        echo '<script>alert("data saving failed!");</script>';
+        //echo "<script>alert('Error in registration'); window.location.href='signup.html';</script>";
+        //echo "Error in registration";
+    }
+}
+
