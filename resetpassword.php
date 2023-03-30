5a67c9f14a809dabecfbab65fcf9362f6ddde159
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-30 23:27:39 +0545

    user resetpassword version 1.2

--- resetpassword.php
+++ resetpassword.php
@@ -12,6 +12,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Get form data
+$email = $_POST['email'];
+$new_password = $_POST['new_password'];
+
+// Query database for user with matching email
+$sql = "SELECT * FROM user WHERE email='$email'";
+$result = $conn->query($sql);
+
 
 
 $conn->close();
