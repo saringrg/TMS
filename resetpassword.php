2ccda6ae90e066e43bbff9837fe9b46284aa6389
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-30 23:16:05 +0545

    user resetpassword version 1

--- resetpassword.php
+++ resetpassword.php
@@ -7,35 +7,6 @@ $dbname = "tms";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// Get form data
-$email = $_POST['email'];
-$new_password = $_POST['new_password'];
-
-// Query database for user with matching email
-$sql = "SELECT * FROM user WHERE email='$email'";
-$result = $conn->query($sql);
-
-// If email matches a user in the database, update their password
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $user_id = $row['id'];
-    $sql = "UPDATE user SET password='$new_password' WHERE id='$user_id'";
-    if ($conn->query($sql) === TRUE) {
-        echo '<script>alert("Password updated successfully.");</script>';
-        // redirect to login page
-        echo '<script>window.location.href = "login.html";</script>';
-
-    } else {
-        echo "<script>alert('Error updating password'); window.location.href='resetpassword.html';</script>";
-    }
-} else {
-    echo "<script>alert('Email not found in database.'); window.location.href='resetpassword.html';</script>";
-}
 
 $conn->close();
 ?>
