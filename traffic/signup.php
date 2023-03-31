83e95a4d0f6c6d97f7efc5cc4345f8bdde6ef85c
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-31 12:46:20 +0545

    bug 1

--- traffic/signup.php
+++ traffic/signup.php
@@ -7,24 +7,5 @@
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','tms');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into traffic(fullname, email, password, batchnumber) values(?, ?, ?, ?)");
-		$stmt->bind_param("sssi", $fullname, $email, $password, $batchnumber);
-		$execval = $stmt->execute();
-		$stmt->close();
-		$conn->close();
-
-		if($execval) {
-			// show pop-up message
-			echo '<script>alert("Account created successfully!");</script>';
-			// redirect to login page
-			echo '<script>window.location.href = "login.html";</script>';
-		} else {
-			echo "<script>alert('Error in registration'); window.location.href='signup.html';</script>";
-			//echo "Error in registration";
-		}
-	}
+	
 ?>
