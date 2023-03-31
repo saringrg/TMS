366a0492c4fe7718771eb879382f44b9cf1dd076
sarin <np03cs4a210050@heraldcollege.edu.np>
2023-03-31 13:00:45 +0545

    bug 1.1

--- traffic/signup.php
+++ traffic/signup.php
@@ -7,5 +7,19 @@
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','tms');
-	
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		
+		if($execval) {
+			// show pop-up message
+			echo '<script>alert("Account created successfully!");</script>';
+			// redirect to login page
+			echo '<script>window.location.href = "login.html";</script>';
+		} else {
+			echo "<script>alert('Error in registration'); window.location.href='signup.html';</script>";
+			//echo "Error in registration";
+		}
+	}
 ?>
