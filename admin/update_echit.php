3c4322423bd3354bed833b22a2366a002d1b30a3
Yajan Gurung <np03cs4a210061@heraldcollege.edu.np>
2023-05-14 13:41:15 +0545

    re echit php code

--- admin/update_echit.php
+++ admin/update_echit.php
@@ -5,17 +5,17 @@ if (isset($_POST['submit'])) {
   $conn = mysqli_connect("localhost", "root", "", "tms");
 
   // Retrieve form data
-  // $id = mysqli_real_escape_string($conn, $_POST['id']);
-  // $fname = mysqli_real_escape_string($conn, $_POST['name']);
-  // $email = mysqli_real_escape_string($conn, $_POST['email']);
-  // $date = mysqli_real_escape_string($conn, $_POST['date']);
-  // $license_number = mysqli_real_escape_string($conn, $_POST['license_number']);
-  // $fine_box = mysqli_real_escape_string($conn, $_POST['fine_box']);
-  // $traffic_station = mysqli_real_escape_string($conn, $_POST['traffic_station']);
-  // $chit_number = mysqli_real_escape_string($conn, $_POST['chit_number']);
-  // $payment = mysqli_real_escape_string($conn, $_POST['payment']);
+  $id = mysqli_real_escape_string($conn, $_POST['id']);
+  $fname = mysqli_real_escape_string($conn, $_POST['name']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $date = mysqli_real_escape_string($conn, $_POST['date']);
+  $license_number = mysqli_real_escape_string($conn, $_POST['license_number']);
+  $fine_box = mysqli_real_escape_string($conn, $_POST['fine_box']);
+  $traffic_station = mysqli_real_escape_string($conn, $_POST['traffic_station']);
+  $chit_number = mysqli_real_escape_string($conn, $_POST['chit_number']);
+  $payment = mysqli_real_escape_string($conn, $_POST['payment']);
 
-  // // Update the data in the database
+  // Update the data in the database
   $query = "UPDATE echit SET name='$fname', email='$email', date='$date', license_number='$license_number', fine_box='$fine_box', traffic_station='$traffic_station', chit_number='$chit_number', payment='$payment' WHERE id='$id'";
   $result = mysqli_query($conn, $query);
 
